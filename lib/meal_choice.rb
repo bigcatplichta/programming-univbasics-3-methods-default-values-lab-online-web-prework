9e2d3c8efe027b03aa12969e245f2390b9d86e80
Matt Plichta <bigcatplichta@gmail.com>
2019-08-10 15:54:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,12 +1,6 @@
 def meal_choice(veg1, veg2, protein = "meat")
-  "What a nutritious meal!"
+  puts "What a nutritious meal!"
   final_menu = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts final_menu
   final_menu
-end
-
-def tryitout()
-  mymealmsg = meal_choice("eggplant", "broccoli", "beef")
-  
-  puts "I decided I want my meal to be '#{mymealmsg}'"
 end
