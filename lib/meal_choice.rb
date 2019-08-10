077f77d7eef276abaf3c61f64a07ff8ec4a8f79d
Matt Plichta <bigcatplichta@gmail.com>
2019-08-10 16:09:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,10 @@ def meal_choice(veg1, veg2, protein = "meat")
   print_return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-def print_return(msg)
+def print_return(msg, x=10)
+  if x < 0 then
+    msg = "Why would you do this!!!!: #{msg}"
+  end
   puts msg
   msg
 end
